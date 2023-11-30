1fe680e73b7240f75a9444c20b9f0b6dba75dd94
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-11-30 20:51:36 +0600

    fixed bug

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth,signOut } from "firebase/auth";
 
 const axiosSecure=axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://survey-sphere-server-side.vercel.app",
     withCredentials:true
 })
 
